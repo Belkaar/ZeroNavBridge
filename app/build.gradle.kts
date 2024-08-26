3a8a7050ec45b972b2783dbd24062cb58934d5ed
Belkaar <2395990+Belkaar@users.noreply.github.com>
2024-08-26 22:28:29 +0200

    Set app version to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 		applicationId = "team.burkart.zero.navbridge"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 3
-		versionName = "1.2"
+		versionCode = 4
+		versionName = "1.3"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
